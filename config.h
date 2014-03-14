0d4a2bf4370a4515d36825e5bf95fcf585e64bc3
Chris Luke <chris_luke@cable.comcast.com>
2014-03-13 22:24:27 -0400

    Tweaks to make it work with most servos.
    In particular, it targets a specific pulse timing for the full
    range of the input signal. The useful range for most servos is
    about 1ms to 2ms of a pulse. The code calculates the desired
    pulse length and then converts this into the PWM timing
    necessary. This uses floating point for now, we may want to use
    integer math later for performance.
    
    We drop the processor clock to 1MHz and set the PWM counter such that
    we get something like a 16ms PWM period. This may be too quick for
    some servos but seems to work for this.
    
    We also setup some registers that were missing.

--- config.h
+++ config.h
@@ -20,20 +20,27 @@ extern "C" {
 #endif
 
 #if defined(__12F683)
-#pragma config FCMEN = OFF          // Failsafe clock monitor
+#pragma config FCMEN = ON           // Failsafe clock monitor
 #pragma config IESO = OFF           // Internal/external osc switchover
-//#pragma config BODEN = 0x0          // Brown-out detect off
+#pragma config BOREN = 0x0          // Brown-out detect off
 #pragma config CPD = OFF            // Data EEPROM protection
 #pragma config CP = OFF             // Code EEPROM protection
 #pragma config MCLRE = OFF          // !MCLR disable
-#pragma config PWRTE = OFF          // Power up timer disable
+#pragma config PWRTE = ON           // Power up timer disable
 #pragma config WDTE = OFF           // Watchdog timer disable
-#pragma config FOSC = 0x6           // Internal clock, I/O on RA4/RA5
+#pragma config FOSC = 0b100         // Internal clock, I/O on RA4/RA5
 #else
 #error This code is designed for a PIC12F683!
 #endif
 
-#define _XTAL_FREQ 8000000
+
+/* With a processor clock of 1MHz and 16:1 prescaler, PR2 0xff gives
+ * us a 16.38ms PWM period (aka  61Hz) */
+#define SET_PR2 (0xff)          /* Maximum period - 16.38ms */
+
+#define SET_IRCF (0b100)        /* 1MHz */
+#define _XTAL_FREQ (1000000)    /* 1MHz */
+
 
 #ifdef	__cplusplus
 }
--- servo_driver.c
+++ servo_driver.c
@@ -9,9 +9,6 @@
 #include "config.h"
 #include <xc.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-
 
 /*
  * Simple loop to read ADC value and set PWM duty
@@ -19,56 +16,91 @@
  * GP0 is the analog input. Pin 7.
  * GP2 is the PWM outut. Pin 5.
  */
-int main(int argc, char** argv) {
-    TRISIO = 0x00; /* Default all IO to input */
+int main(int argc, char **argv) {
+    unsigned int period;
+
+    /* Set the clock speed */
+    OSCCONbits.IRCF = SET_IRCF;
+    WDTCON = 0x0; /* Make sure watchdog is disabled */
+
+    di(); /* No need for interrupts */
+    GPIO = 0; /* Make sure all outputs are low */
+    CMCON0bits.CM = 0b111; /* Disable comparators */
+    TRISIO = 0b111111; /* Default all IO to input */
+    nGPPU = 0; /* Enable pullups */
 
     /* Setup ADC */
-
     ADCON0bits.ADFM = 1; /* right justified result */
     ADCON0bits.VCFG = 0; /* Use Vdd as Vref */
-
-    /* We only need one ADC input */
-    ADCON0bits.CHS = 0;
-    ANSELbits.ANS = 1;
-    TRISIObits.TRISIO0 = 1;
-    
-    /* 16 TOSC on an 8MHz internal clock gives us 2us
-     * sample time. We need minimum of 1.6us */
-    ANSELbits.ADCS = 0b101;
-
+    WPU0 = 0; /* Disable pullup on this input */
+    ADCON0bits.CHS = 0; /* Select channel 0 */
+    GO_DONE = 0; /* Make sure this is cleared */
+    ANSELbits.ANS = 1; /* Only channel 0 is enabled */
+    ANSELbits.ADCS = 0b111; /* Use internal RC oscilator */
     ADON = 1; /* Emable the ADC */
 
     /* Setup PWM */
-    TRISIObits.TRISIO2 = 0; /* Output pin */
-    PR2 = 0xff; /* Slowest PWM possible */
+    TRISIO2 = 1; /* Disable output on this pin for now */
+    PR2 = SET_PR2; /* Set the PWM period (we aim for ~20ms, see config.h)  */
+    CCP1CONbits.CCP1M = 0b1100; /* PWM mode */
+    CCPR1L = 0; /* Reset PWM output to 0 */
     CCP1CONbits.DC1B = 0;
-    CCPR1L = 0;
+
+    /* Set the timer up for PWM operation */
+    TMR2IF = 0; /* Make sure this is clear */
     T2CONbits.T2CKPS = 2; /* Timer 2 16:1 prescale */
     T2CONbits.TMR2ON = 1; /* TImer 2 on */
-    CCP1CONbits.CCP1M = 0b1100; /* PWM mode */
+
+    while(!TMR2IF) { /* Wait for Timer 2 */
+        continue;
+    }
+    TRISIO2 = 0; /* Set the pin as output */
+
+    /* Calculate the PWM period from what we set PR2 to.
+     * This will be ms*100, something like 1638 (61Hz) */
+    period = (unsigned int)(
+            ( ((float)SET_PR2) + 1.0 ) *
+            4.0 *
+            (1.0/(float)_XTAL_FREQ) *
+            16.0 *
+            1000.0 *
+            100.0
+    );
 
     /* Let it all settle */
     __delay_ms(1);
     
     for(;;) { /* Loop forver */
-        unsigned int value;
+        unsigned int value, pulse;
         
-        /* Trigger ADC */
-        ADCON0bits.CHS = 0;
-        ADCON0bits.GO = 1;
+        /* Trigger ADC. */
+        GO_DONE = 1;
 
-        /* Wait for conversion */
-        while(ADCON0bits.GO);;
+        /* Wait for conversion to complete. */
+        while(GO_DONE) {
+            continue;
+        }
 
         /* Read ADC */
         value = ADRESH << 8;
         value |= ADRESL;
 
-        /* Set PWM */
-        CCP1CONbits.DC1B = value & 0x03; /* Two LSB's */
-        CCPR1L = (value >> 2) & 0xff; /* Eight LSB's */
+        /* Calculate the target pulse width in ms*100.
+         * Useful range for most servos is 1ms to 2ms.
+         * The values here give us a bit either side, just in case */
+        pulse = 93 + (value / 9);
 
-        /* Wait a while - mostly to let ADC reset */
+        /* Now adjust for the period of the timer.
+         * We use floating point math for now, for accuracy.
+         * Ideally we'd use integer math for speed, though we seem to
+         * have enough cycles and memory for this. */
+        pulse = (unsigned int)(1024.0 / ((float)period / (float)pulse));
+
+        /* Set PWM */
+        CCP1CONbits.DC1B = pulse & 0x03; /* Two LSB's */
+        CCPR1L = (pulse >> 2) & 0xff; /* Eight MSB's */
+
+        /* Wait a while - mostly to let ADC reset. */
         __delay_us(100);
     }
 }
